656e6af307194ec05c7361855cd91b38646ac220
Francis J. Aparicio <fjapariciod@gmail.com>
2018-04-07 18:11:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if (string.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can\'t hear you!"
-  } else if (string.toUpperCase() === uppercase) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else {
     return "I love you, too."
